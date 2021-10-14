7937fbe0ad2c69004c5e9790f58cf016ca84a584
p1809208 <younes.derbal@etu.univ-lyon1.fr>
2021-10-14 20:22:19 +0200

    creation de filtre Autorisation #9

--- server/src/main/java/fr/univlyon1/m1if/m1if03/p1507426/servlets/ServletController.java
+++ server/src/main/java/fr/univlyon1/m1if/m1if03/p1507426/servlets/ServletController.java
@@ -1,8 +1,38 @@
-package fr.univlyon1.m1if.m1if03.p1507426.servlets;
-
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
-
-@WebServlet(name = "ServletController", urlPatterns = {"/election/resultat", "/election/user", "/election/vote","/election/listBallots"})
-public class ServletController extends HttpServlet {
-}
+//package fr.univlyon1.m1if.m1if03.p1507426.servlets;
+//
+//import javax.servlet.ServletException;
+//import javax.servlet.annotation.WebServlet;
+//import javax.servlet.http.HttpServlet;
+//import javax.servlet.http.HttpServletRequest;
+//import javax.servlet.http.HttpServletResponse;
+//import java.io.IOException;
+//
+//@WebServlet(name = "ServletController", urlPatterns = {"/election/resultat", "/election/user", "/election/vote","/election/listBallots"})
+//public class ServletController extends HttpServlet {
+//    @Override
+//    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+//        String uri = req.getRequestURL().toString().split("/election")[1];;
+//        System.out.println("uri : " + uri );
+//        switch (uri){
+//            case "/resultat":
+//                System.out.println("resultat");
+////                resp.sendRedirect("/resultats");
+//                req.getRequestDispatcher("/resultats").include(req, resp);;
+//
+//                break;
+//            case "/election/user":
+//
+//                break;
+//
+//            default:
+//                resp.sendError(HttpServletResponse.SC_FORBIDDEN, "Voila je suis dans la servlet ");
+//                break;
+//        }
+//
+//    }
+//
+//    @Override
+//    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+//        super.doPost(req, resp);
+//    }
+//}
