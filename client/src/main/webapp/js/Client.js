b3ef3e2a214bf12e0c4ead9878e1b9e450be4a17
p1809208 <younes.derbal@etu.univ-lyon1.fr>
2022-01-09 20:41:42 +0100

    derniere version de l'appli

--- client/src/main/webapp/js/Client.js
+++ client/src/main/webapp/js/Client.js
@@ -1,4 +1,4 @@
-let apiURL = "https://192.168.75.70/api/v3";
+let apiURL = "https://192.168.75.56/api/v3";
 let currentUser = {
     login: "",
     nom: "",
@@ -8,6 +8,13 @@ let ballot = {
     votant: "",
     id: ""
 };
+let messageError = {
+    errorMessage: "",
+};
+let modaleWindows = {
+    titre: "",
+    messageModal: ""
+}
 let listCandidats = [];
 let resultats = [];
 let token;
@@ -16,13 +23,12 @@ let token;
 
 
 function login() {
-
-    (function () {
+    (function() {
         'use strict'
         var forms = document.querySelectorAll('.needs-validation')
         Array.prototype.slice.call(forms)
-            .forEach(function (form) {
-                form.addEventListener('submit', function (event) {
+            .forEach(function(form) {
+                form.addEventListener('submit', function(event) {
                     if (!form.checkValidity()) {
                         event.preventDefault()
                         event.stopPropagation()
@@ -35,38 +41,34 @@ function login() {
                             "admin": $("#adminForm").get(0).checked
                         };
                         fetch(apiURL + '/users/login', {
-                            method: "POST",
-                            headers: { 'Content-Type': 'application/json' },
-                            body: JSON.stringify(userData),
-                            credentials: "same-origin",
-                            mode: "cors"
-                        })
+                                method: "POST",
+                                headers: { 'Content-Type': 'application/json' },
+                                body: JSON.stringify(userData),
+                                credentials: "same-origin",
+                                mode: "cors"
+                            })
                             .then(response => {
                                 token = response.headers.get("Authorization")
-                                console.log("reponse :  " + token);
-                                console.log("userId: " + getUserLogin(token));
-                                getUserProfil(getUserLogin(token));
+                                console.log("response Token:  " + token);
+                                console.log("login: " + getLoginUser(token));
+                                getProfileUser(getLoginUser(token));
                                 window.location.hash = "#monCompte";
                                 $("#connecte").hide();
 
                             })
                             .catch(error => console.log(error));
 
-
                     }
-
                     form.classList.add('was-validated')
                 }, false)
             })
     })()
-
-
     let userData;
 }
 
 
 function putNom(balise) {
-    let userId = getUserLogin(token);
+    let userId = getLoginUser(token);
     let userNom;
     if (balise === "nom") {
         userNom = {
@@ -91,14 +93,14 @@ function putNom(balise) {
             if (response.ok) {
                 $("#nomPut").val('');
                 currentUser.nom = userNom.nom;
-                getUserProfil(userId);
+                getProfileUser(userId);
             }
 
         })
         .catch(error => { console.log(error); })
 }
 
-function getUserProfil(login) {
+function getProfileUser(login) {
     fetch(apiURL + '/users/' + login, {
             method: "GET",
             headers: {
@@ -131,8 +133,8 @@ function logout() {
         })
         .then(response => {
             if (response.ok){
-                $("#loginForm").val('') ;
-                $("#nomForm").val('') ;
+                $("#loginForm").val('');
+                $("#nomForm").val('');
                 token = null;
                 currentUser = {
                     login: "",
@@ -147,10 +149,9 @@ function logout() {
                 showMenuConnecte();
             }
 
+
         })
-        .catch(error => {
-            console.log(error)
-        });
+        .catch(error => {console.log(error)});
 }
 
 function getCandidat(candidatId) {
@@ -170,13 +171,12 @@ function getCandidat(candidatId) {
         })
         .then(data => {
             if (data == null) {
-                const message = {
-                    msg: "Vous devez connectez pour voir les informations de ce candidat !",
+                messageError = {
+                    errorMessage: "Connectez-vous pour avoir accès aux informations de ce candidat."
                 };
                 $('#target-output-candidat-info').hide();
                 $('#target-output-candidat-non-connect').show();
-                showTemplateData('mustacheTempalte_candidat-non-connect', message, 'target-output-candidat-non-connect');
-
+                showTemplateData('mustacheTempalte_candidat-non-connect', messageError, 'target-output-candidat-non-connect');
             } else {
 
                 $('#target-output-candidat-non-connect').hide();
@@ -219,7 +219,6 @@ function getResultats() {
             method: "GET",
             headers: {
                 'Accept': "application/json",
-
             },
             mode: "cors",
             credentials: "same-origin"
@@ -231,6 +230,7 @@ function getResultats() {
         .then(data => {
             resultats = data;
             window.location.hash = "#index";
+            // showResultats();
             showTemplateData('mustacheTempalte_resultats', resultats, 'target-output-resultats')
             show('#index');
         })
@@ -238,7 +238,7 @@ function getResultats() {
 }
 
 function vote() {
-    const nomCandidat = {
+    let nomCandidat = {
         "nomCandidat": $('#candidat-select option:selected').text()
     }
     fetch(apiURL + '/election/ballots', {
@@ -253,20 +253,19 @@ function vote() {
         })
         .then(response => {
                 if (response.ok) {
-                    let modale = {
+                     modaleWindows = {
                         titre: "Informations vote ",
-                        msg: "Vous avez bien voté "
-
+                        messageModal: "Vous avez bien voté "
                     }
-                    showTemplateData('mustacheTempalte_fenetre_modale', modale, 'target-output-modal')
+                    showTemplateData('mustacheTempalte_fenetre_modale', modaleWindows, 'target-output-modal')
 
                 } else {
-                    let modale = {
+                     modaleWindows = {
                         titre: "Informations vote ",
-                        msg: "Votre vote n'est pas passée"
+                        messageModal: "Votre vote n'est pas passée"
 
                     }
-                    showTemplateData('mustacheTempalte_fenetre_modale', modale, 'target-output-modal')
+                    showTemplateData('mustacheTempalte_fenetre_modale', modaleWindows, 'target-output-modal')
 
                 }
             }
@@ -276,7 +275,7 @@ function vote() {
 }
 
 function getBallot() {
-    let userId = getUserLogin(token);
+    let userId = getLoginUser(token);
     fetch(apiURL + '/election/ballots/byUser/' + userId, {
             method: 'GET',
             headers: {
@@ -293,13 +292,15 @@ function getBallot() {
             }
         })
         .then(data => {
+            console.log("DATA : " + JSON.stringify(data));
             if (data == null) {
-                let modale = {
-                    msg: "Vous n'avez pas encore voté "
+                 messageError = {
+                    errorMessage: "Vous n'avez pas encore voté ! "
+
                 };
                 $('#target-output-ballot').hide();
                 $('#target-output-ballot-non-vote').show();
-                showTemplateData('mustacheTempalte_ballot-non-vote', modale, 'target-output-ballot-non-vote');
+                showTemplateData('mustacheTempalte_ballot-non-vote', messageError, 'target-output-ballot-non-vote');
             } else {
                 ballot = data;
                 $('#target-output-ballot-non-vote').hide();
@@ -307,7 +308,6 @@ function getBallot() {
                 showTemplateData('mustacheTempalte_ballot', ballot.id.split("/ballots/")[1], 'target-output-ballot');
                 return ballot;
             }
-
         })
         .catch(error => console.error(error));
 }
@@ -324,20 +324,20 @@ function deleteVote() {
         })
         .then(response => {
             if (response.status === 204) {
-                let modale = {
+                 modaleWindows = {
                     titre: "Informations vote ",
-                    msg: "Votre vote a bien été supprimé "
+                    messageModal: "Votre vote a bien été supprimé "
 
                 }
-                showTemplateData('mustacheTempalte_fenetre_modale', modale, 'target-output-modal');
+                showTemplateData('mustacheTempalte_fenetre_modale', modaleWindows, 'target-output-modal');
                 show("#ballot");
-            } else if (response.status === 404) {
-                let modale = {
+            } else  {
+                 modaleWindows = {
                     titre: "Informations vote ",
-                    msg: "Vous n'avez pas encore voté "
+                    messageModal: "Vous ne pouvez pas supprimer un vote ! vous n'avez pas encore voté "
 
                 }
-                showTemplateData('mustacheTempalte_fenetre_modale', modale, 'target-output-modal');
+                showTemplateData('mustacheTempalte_fenetre_modale', modaleWindows, 'target-output-modal');
             }
         })
         .catch(error => console.log(error));
@@ -357,47 +357,10 @@ function getLoginUri(uri) {
     return splitedUri[splitedUri.length - 1];
 }
 
-function getUserLogin(token) {
+function getLoginUser(token) {
     let tokenM = token.replace("Bearer ", "");
     let sub = splitToken(tokenM).payload.sub;
     let userLogin = getLoginUri(sub);
     console.log("USER ID : " + userLogin);
     return userLogin;
 }
-
-document.addEventListener("DOMContentLoaded", function(event) {
-
-    const showNavbar = (toggleId, navId, bodyId, headerId) => {
-        const toggle = document.getElementById(toggleId),
-            nav = document.getElementById(navId),
-            bodypd = document.getElementById(bodyId),
-            headerpd = document.getElementById(headerId)
-
-        // Validate that all variables exist
-        if (toggle && nav && bodypd && headerpd) {
-            toggle.addEventListener('click', () => {
-                // show navbar
-                nav.classList.toggle('show')
-                    // change icon
-                toggle.classList.toggle('bx-x')
-                    // add padding to body
-                bodypd.classList.toggle('body-pd')
-                    // add padding to header
-                headerpd.classList.toggle('body-pd')
-            })
-        }
-    }
-
-    showNavbar('header-toggle', 'nav-bar', 'body-pd', 'header')
-
-    /*===== LINK ACTIVE =====*/
-    const linkColor = document.querySelectorAll('.nav_link')
-
-    function colorLink() {
-        if (linkColor) {
-            linkColor.forEach(l => l.classList.remove('active'))
-            this.classList.add('active')
-        }
-    }
-    linkColor.forEach(l => l.addEventListener('click', colorLink))
-});
--- client/src/main/webapp/js/Spa.js
+++ client/src/main/webapp/js/Spa.js
@@ -1,15 +1,15 @@
+let idCandidat = null
 
-let idCandiat = null
 function displayNavbarConnection() {
 
-    $('#menu1').on("click", function () {
+    $('#menu1').on("click", function() {
         $('#menuA').slideToggle('blink');
     });
     $('#nom').attr('contentEditable', 'true')
 
 }
-function showMenuConnecte() {
 
+function showMenuConnecte() {
     if (currentUser.login !== "") {
         $('#menuConnecte').show();
         $('#menuConnexion').hide();
@@ -20,54 +20,55 @@ function showMenuConnecte() {
     }
 
 }
+
 function show(hash) {
     $('.active').removeClass('active').addClass('inactive');
     $(hash).removeClass('inactive').addClass('active');
-    console.log("hash: " + hash);
     switch (hash) {
         case "#monCompte":
-            if (token != null){
-                let userId = getUserLogin(token);
-                getUserProfil(userId);
-                $('#target-output').on('click', function (){
+            if (token != null) {
+                let userId = getLoginUser(token);
+                getProfileUser(userId);
+                $('#target-output').on('click', function() {
                     $('#nom').attr('contenteditable', 'true');
                 });
-                $('#target-output').on('keypress',function (e){
-                    if(e.which == 13) {
+                $('#target-output').on('keypress', function(e) {
+                    if (e.which == 13) {
                         putNom("nom");
                     }
                 });
-
-             } else {
+            } else {
                 show("#index");
             }
-
             break;
+
         case "#candidats":
             getListCandidats();
             break;
-        case "#index" :
-            setTimeout( function (){
-                if (window.location.hash === '#index'){
+        case "#index":
+            setTimeout(function() {
+                if (window.location.hash === '#index') {
                     getResultats();
-                }},5000);
+                }
+            }, 5000);
             break;
-        case "#vote" :
-            if (token !=null){
+        case "#vote":
+            if (token != null) {
                 getListCandidats();
-            }else {
+            } else {
                 show("#index")
             }
+
             break;
-        case "#ballot" :
-            if (token !=null) {
+        case "#ballot":
+            if (token != null) {
                 getBallot();
-            }else {
+            } else {
                 show("#index")
             }
             break;
         case "#candidat":
-            getCandidat(idCandiat);
+            getCandidat(idCandidat);
             break;
         default:
             console.log("DEFAULT");
@@ -80,13 +81,16 @@ function show(hash) {
 $(document).ready(function() {
 
     window.addEventListener('hashchange', () => {
-        if (window.location.hash.split("/")[0] === "#candidat"){
-            idCandiat = window.location.hash.split("/")[1]
+        console.log("hash document ready: " + window.location.hash.split("/")[0]);
+        if (window.location.hash.split("/")[0] === "#candidat") {
+            console.log("J'y suis");
+            idCandidat = window.location.hash.split("/")[1]
             show(window.location.hash.split("/")[0]);
         } else {
             show(window.location.hash);
         }
     });
+
     showMenuConnecte();
     getResultats();
 
@@ -94,3 +98,39 @@ $(document).ready(function() {
 
 });
 
+document.addEventListener("DOMContentLoaded", function(event) {
+
+    const showNavbar = (toggleId, navId, bodyId, headerId) => {
+        const toggle = document.getElementById(toggleId),
+            nav = document.getElementById(navId),
+            bodypd = document.getElementById(bodyId),
+            headerpd = document.getElementById(headerId)
+
+        // Validate that all variables exist
+        if (toggle && nav && bodypd && headerpd) {
+            toggle.addEventListener('click', () => {
+                // show navbar
+                nav.classList.toggle('show')
+                    // change icon
+                toggle.classList.toggle('bx-x')
+                    // add padding to body
+                bodypd.classList.toggle('body-pd')
+                    // add padding to header
+                headerpd.classList.toggle('body-pd')
+            })
+        }
+    }
+
+    showNavbar('header-toggle', 'nav-bar', 'body-pd', 'header')
+
+    /*===== LINK ACTIVE =====*/
+    const linkColor = document.querySelectorAll('.nav_link')
+
+    function colorLink() {
+        if (linkColor) {
+            linkColor.forEach(l => l.classList.remove('active'))
+            this.classList.add('active')
+        }
+    }
+    linkColor.forEach(l => l.addEventListener('click', colorLink))
+});
